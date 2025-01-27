e8d91d36a11596182fa6b002518f0c33988b9589
byteAbhi <abhishekmukhiya56321@gmail.com>
2025-01-27 10:25:54 +0530

    make changes in contactus page , made responive , commected dropdown list of service section

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './components/HomePage/HomePage';
 import AboutUs from './components/AboutUS/AboutUs';
+// import ContactPageHp from './components/Contact-form-HP/ContactPageHp';
+
 // import NavBar from './components/NavBar/NavBar'
 import ServicesPage from './components/ServicePage/Service';
+import ContactUs from './components/Contact-form-HP/ContactUs';
 
 function App() {
   return (
     <BrowserRouter>
-    {/* <NavBar /> */}
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-<<<<<<< HEAD
-      <Route path="/about-us" element={<AboutUs/>}/>
-=======
-      {/* <Route path="/About" element{</>} */}
-      <Route path="/service" element={<ServicesPage />} />
->>>>>>> 3d3dbde6324a114c0b52a702867d5958503e0946
-    </Routes>
-  </BrowserRouter>
+      {/* <NavBar /> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/service" element={<ServicesPage />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
